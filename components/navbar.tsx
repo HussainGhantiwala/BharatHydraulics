7bd2d91e3f24a8cd654ab29d7f15eb33477e93a4
YusufDeesawala <yusufdeesawala72@gmail.com>
2025-06-10 00:02:31 +0530

    Updated navbar to show logo instead of wrench placeholder.

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -24,8 +24,8 @@ export function Navbar() {
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
           <Link href="/" className="flex items-center space-x-2">
-            <div className="w-8 h-8 bg-teal-600 dark:bg-green-600 rounded-lg flex items-center justify-center">
-              <Wrench className="h-5 w-5 text-white" />
+            <div className="w-8 h-8 bg-white-600 dark:bg-green-600 rounded-lg flex items-center justify-center">
+              <img src="/Image/bh-logo.png" alt="Logo" className="h-5 w-5" />
             </div>
             <span className="text-xl font-bold text-gray-900 dark:text-white">Bharat Hydraulics & Engineering Co.</span>
           </Link>
